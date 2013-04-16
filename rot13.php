2161319aff98cb167492ed20a9840f19c89f50fc
Jose Ramon <joseoviedo.do@gmail.com>
2013-04-15 23:47:27 -0300

    Update rot13.php

--- rot13.php
+++ rot13.php
@@ -16,7 +16,7 @@ function rot13($myStr) {
         $posChr   = FALSE;
         
         if (($posChr = strpos($arrAbs, $chrUpper))) {
-            $index = ($posChr < (26 / 2)) ? ((26 / 2) + $posChr) : ($posChr - 13);
+            $index = ($posChr <= (26 / 2)) ? ((26 / 2) + $posChr) : ($posChr - 13);
             $newArray = str_split($arrAbs);
             $newChr = $newArray{$index};
             $chr = (ord($chrUpper) >= ord($chr)) ? strtoupper($newChr): strtolower($newChr);
